bd05186bfd4d3d476fc08907f2322e3b8019a42a
Yusuke Matsui <matsui528@gmail.com>
2019-10-16 15:33:42 +0000

    detelte tf.graph because that is removed on tf2

--- feature_extractor.py
+++ feature_extractor.py
@@ -2,14 +2,12 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
 import numpy as np
-import tensorflow as tf
 
 
 class FeatureExtractor:
     def __init__(self):
         base_model = VGG16(weights='imagenet')
         self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
-        self.graph = tf.get_default_graph()
 
     def extract(self, img):  # img is from PIL.Image.open(path) or keras.preprocessing.image.load_img(path)
         img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
@@ -18,6 +16,5 @@ class FeatureExtractor:
         x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C), where the first elem is the number of img
         x = preprocess_input(x)  # Subtracting avg values for each pixel
 
-        with self.graph.as_default():
-            feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
-            return feature / np.linalg.norm(feature)  # Normalize
+        feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
+        return feature / np.linalg.norm(feature)  # Normalize
