ad5d4bfd548c8b327ae9d87262e1ff6b2c66b7d7
Yusuke Matsui <ymatsui@iis.u-tokyo.ac.jp>
2020-06-07 00:05:43 +0900

    updated for cvpr2020 tutorial

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
-import os
 import numpy as np
 from PIL import Image
 from feature_extractor import FeatureExtractor
-import glob
-import pickle
 from datetime import datetime
 from flask import Flask, request, render_template
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -13,9 +11,10 @@ app = Flask(__name__)
 fe = FeatureExtractor()
 features = []
 img_paths = []
-for feature_path in glob.glob("static/feature/*"):
-    features.append(pickle.load(open(feature_path, 'rb')))
-    img_paths.append('static/img/' + os.path.splitext(os.path.basename(feature_path))[0] + '.jpg')
+for feature_path in Path("./static/feature").glob("*.npy"):
+    features.append(np.load(feature_path))
+    img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
+features = np.array(features)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,13 +22,15 @@ def index():
     if request.method == 'POST':
         file = request.files['query_img']
 
+        # Save query image
         img = Image.open(file.stream)  # PIL image
-        uploaded_img_path = "static/uploaded/" + datetime.now().isoformat() + "_" + file.filename
+        uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
         img.save(uploaded_img_path)
 
+        # Run search
         query = fe.extract(img)
-        dists = np.linalg.norm(features - query, axis=1)  # Do search
-        ids = np.argsort(dists)[:30] # Top 30 results
+        dists = np.linalg.norm(features-query, axis=1)  # L2 distances to features
+        ids = np.argsort(dists)[:30]  # Top 30 results
         scores = [(dists[id], img_paths[id]) for id in ids]
 
         return render_template('index.html',
@@ -37,6 +38,7 @@ def index():
                                scores=scores)
     else:
         return render_template('index.html')
+
 
 if __name__=="__main__":
     app.run("0.0.0.0")
