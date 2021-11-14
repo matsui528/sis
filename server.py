48174b5faca047eaacaae7b3ee4730e93d696fc8
duckimann <facoder2k2@gmail.com>
2021-11-14 14:07:08 +0700

    update search

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from feature_extractor import FeatureExtractor
 from datetime import datetime
 from flask import Flask, request, render_template
 from pathlib import Path
+import re, urllib.parse
 
 app = Flask(__name__)
 
@@ -11,9 +12,9 @@ app = Flask(__name__)
 fe = FeatureExtractor()
 features = []
 img_paths = []
-for feature_path in Path("./static/feature").glob("*.npy"):
+for feature_path in Path("./static/feature").rglob("*.npy"):
     features.append(np.load(feature_path))
-    img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
+    img_paths.append(Path("./static/img") / re.sub(r"static[\\\/]feature[\\\/]|[^\\\/]+$", "", str(feature_path)) / (feature_path.stem + ".jpg"))
 features = np.array(features)
 
 
@@ -33,12 +34,9 @@ def index():
         ids = np.argsort(dists)[:30]  # Top 30 results
         scores = [(dists[id], img_paths[id]) for id in ids]
 
-        return render_template('index.html',
-                               query_path=uploaded_img_path,
-                               scores=scores)
+        return render_template('index.html', query_path=uploaded_img_path, scores=scores)
     else:
         return render_template('index.html')
-
 
 if __name__=="__main__":
     app.run("0.0.0.0")
